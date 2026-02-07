4e825f02d60719021482984f93b2af9c89bc2f4f
MkPtz5GN8JCQwXq <you@example.com>
2026-02-07 14:12:19 +0530

    first commit

---
+++ src/main/java/com/btl/orotrades/OrotradesApplication.java
@@ -0,0 +1,13 @@
+package com.btl.orotrades;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrotradesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrotradesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/btl/orotrades/OrotradesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btl.orotrades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrotradesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
